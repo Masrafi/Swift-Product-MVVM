0301f2260fe5b766f9e7aee3a43873fcc7e2d34d
Masrafi <masrafianam@gmail.com>
2024-08-07 20:38:19 +0600

    Initial Commit

---
+++ MVVM Product API/MVVM_Product_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Product_APIApp.swift
+//  MVVM Product API
+//
+//  Created by Md Khorshed Alam on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Product_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
